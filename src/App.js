40bbda2522d617664bf0365f13626d9ca4208352
Rushikesh Hemant Bobade <rushi2001bobade@gmail.com>
2023-09-08 00:54:55 +0530

    Update App.js
    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My First react app
+          My First react appp
           
         </p>
         
